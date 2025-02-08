fa2fa0dd3fed9393b0153a609edd5721928083d8
Artreeus <142108262+Artreeus@users.noreply.github.com>
2025-02-08 14:02:32 +0600

    ssg added

--- src/app/blogs/[blogid]/page.tsx
+++ src/app/blogs/[blogid]/page.tsx
@@ -1,20 +1,30 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from '@/types'; 
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0,3).map((blog: Blog) => ({
+    blogId : blog.id
+  }))
+}
 
 const BlogDetailsPage = async ({
-  params
+  params,
 }: {
   params: Promise<{ blogId: string }>;
 }) => {
-
-  const {blogId} = await params;
-  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const { blogId } = await params;
+  const res = await fetch(`
+    /${blogId}`);
 
   const blog = await res.json();
- 
+
   console.log(blog);
   return (
-    <div>
-      <BlogDetailsCard blog={blog}/>
+    <div className="my-7">
+      <BlogDetailsCard blog={blog} />
     </div>
   );
 };
